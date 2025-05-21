3135536ee29330bd517dbde697c6a729a0e69398
Federico Caltabiano <97410583+FedeCF03@users.noreply.github.com>
2025-05-20 22:46:59 -0300

    login 1er version

--- Alquileres_Express/Alquileres_Express.UI/Program.cs
+++ Alquileres_Express/Alquileres_Express.UI/Program.cs
@@ -1,11 +1,25 @@
 using Alquileres_Express.UI.Components;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+{
+    options.Cookie.Name = "cookie_auth";
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+    options.SlidingExpiration = true;
+    options.AccessDeniedPath = "/";
+    options.LoginPath = "/login";
 
+});
+builder.Services.AddAuthorization();
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddCascadingAuthenticationState();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,4 +35,8 @@ app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
+builder.Services.AddAuthorization();
 app.Run();
