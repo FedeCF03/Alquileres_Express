636ce00aee2d4d5d9de65d2df6f14327c94f5c71
Federico Caltabiano <97410583+FedeCF03@users.noreply.github.com>
2025-05-20 23:42:28 -0300

    asdas

--- Alquileres_Express/Alquileres_Express.Aplicacion/CasosDeUso/CasoDeUsoRegistrarUsuario.cs
+++ Alquileres_Express/Alquileres_Express.Aplicacion/CasosDeUso/CasoDeUsoRegistrarUsuario.cs
@@ -2,9 +2,9 @@ using System;
 
 using Alquileres_Express.Aplicacion.Interfaces;
 using Alquileres_Express.Aplicacion.Entidades;
-namespace Alquileres_Express.Aplicacion.UseCases;
+namespace Alquileres_Express.Aplicacion.CasosDeUso;
 
-public class RegistrarUsuarioUseCase(IRepositorioUsuario repositorio) : UsuarioUseCase(repositorio)
+public class CasoDeUsoRegistrarUsuario(IRepositorioUsuario repositorio) : CasoDeUsoUsuario(repositorio)
 {
     public void Ejecutar(Usuario usuario)
     {
