d7eceb4817a9505f50f53735e4b896f225c82da6
tomic <tomithespeedster@gmal.com>
2025-06-01 15:29:00 -0300

    Co-authored-by: Federico Caltabiano <FedeCF03@users.noreply.github.com>

---
+++ Alquileres_Express.Aplicacion/Servicios/ServicioLoggear.cs
@@ -0,0 +1,18 @@
+
+// using System.Net.Http;
+// using System.Security.Claims;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Authentication.Cookies;
+// using Alquileres_Express.Aplicacion.Entidades;
+
+// namespace Alquileres_Express.Aplicacion.Servicios;
+
+// static class ServicioLoggear
+// {
+
+
+//     public static async Task Loggear(Usuario usuario)
+//     {
+
+//     }
+// }
